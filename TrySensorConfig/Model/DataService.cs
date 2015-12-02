0cfcb75a79b22a6a7ca905792a63c1bc454d1730
John Jesus <john.jesus@raytheon.com>
2015-12-02 16:54:12 -0600

    Successful XML Config loading.

--- TrySensorConfig/Model/DataService.cs
+++ TrySensorConfig/Model/DataService.cs
@@ -4,12 +4,18 @@ namespace TrySensorConfig.Model
 {
     public class DataService : IDataService
     {
+        private readonly SensorConfigService _sensorConfigService;
         public void GetData(Action<DataItem, Exception> callback)
         {
             // Use this to connect to the actual data service
 
             var item = new DataItem("Welcome to MVVM Light");
             callback(item, null);
+        }
+
+        public DataService()
+        {
+            _sensorConfigService = new SensorConfigService();
         }
     }
 }
---
+++ TrySensorConfig/Model/SensorConfigService.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace TrySensorConfig.Model
+{
+    public class SensorConfigService
+    {
+        private bool _isXmlValid = true;
+        private XNamespace _defaultNS = "http://www.example.org/SensorConfig";
+        private string _xsdPath = @"Configuration/SensorConfig.xsd";
+        private string _xmlPath = @"Configuration/SensorConfig.xml";
+
+        public SensorConfigService()
+        {
+            validateXml();
+            if (_isXmlValid)
+            {
+                loadXml();
+            }
+
+        }
+        private void validateXml()
+        {
+            XmlSchemaSet schemaSet = new XmlSchemaSet();
+            try
+            {
+                schemaSet.Add(_defaultNS.NamespaceName, _xsdPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Problem loading xsd schema file");
+                Console.WriteLine(e.Message);
+            }
+            validateXml(_xmlPath, schemaSet);
+        }
+
+        private void validateXml(string filename, XmlSchemaSet schemaSet)
+        {
+            Console.WriteLine();
+            Console.WriteLine("\r\nValidating XML file {0}...", filename.ToString());
+
+            XmlSchema compiledSchema = null;
+
+            foreach (XmlSchema schema in schemaSet.Schemas())
+            {
+                compiledSchema = schema;
+            }
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.Schemas.Add(compiledSchema);
+            settings.ValidationEventHandler += new ValidationEventHandler(validationCallBack);
+            settings.ValidationType = ValidationType.Schema;
+
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessInlineSchema;
+            settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessSchemaLocation;
+
+            //Create the schema validating reader.
+            XmlReader vreader = XmlReader.Create(filename, settings);
+            try
+            {
+                while (vreader.Read()) { }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Validation error: ");
+                Console.WriteLine(e.Message);
+                _isXmlValid = false;
+            }
+            finally
+            {
+                //Close the reader.
+                vreader.Close();
+            }
+        }
+
+        private void validationCallBack(object sender, ValidationEventArgs args)
+        {
+            if (args.Severity == XmlSeverityType.Warning)
+                Console.WriteLine("\tWarning: Matching schema not found.  No validation occurred." + args.Message);
+            else
+            {
+                Console.WriteLine("\tValidation error: " + args.Message);
+                _isXmlValid = false;
+            }
+        }
+
+        private void loadXml()
+        {
+            try
+            {
+                XDocument doc = XDocument.Load(_xmlPath);
+                var sensorConfigs = from item in doc.Descendants(_defaultNS + "SensorConfig")
+                             select item;
+                foreach (var sc in sensorConfigs)
+                {
+                    Console.WriteLine(sc.ToString());
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Problem loading xml file");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+    }
+}
